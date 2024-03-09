87d0e2a74f55f257a56424ac382f0b2523d0ca17
Aadil Srivastava <aadil.srivastava01@gmail.com>
2024-03-09 13:14:16 +0530

    Added a BST, with traverse, lookup and insert operation

---
+++ problems/BST.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+class TreeNode
+{
+public:
+    friend std::ostream &operator<<(std::ostream &os, const TreeNode &obj);
+    friend std::ostream &operator<<(std::ostream &os, const TreeNode *obj);
+
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val) : value{val}, left{NULL}, right{NULL} {};
+};
+
+std::ostream &operator<<(std::ostream &os, const TreeNode &obj)
+{
+    os << obj.value;
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const TreeNode *obj)
+{
+    if (obj == NULL)
+        os << "Not Found";
+    else
+        os << obj->value;
+    return os;
+}
+
+class BinarySearchTree
+{
+private:
+    TreeNode *parent;
+    void iOTraverse(TreeNode *node);
+
+public:
+    TreeNode *root;
+
+    BinarySearchTree(int value);
+    TreeNode *lookup(int x);
+    void insert(int num);
+    void inOrderTraverse() { iOTraverse(root); }
+};
+
+BinarySearchTree::BinarySearchTree(int value) { root = new TreeNode(value); }
+
+TreeNode *BinarySearchTree::lookup(int x)
+{
+    TreeNode *ptr = root;
+    while (ptr != NULL)
+    {
+        if (ptr->value == x)
+            return ptr;
+        else if (x < ptr->value)
+            ptr = ptr->left;
+        else
+            ptr = ptr->right;
+    }
+
+    return NULL;
+}
+
+void BinarySearchTree::insert(int num)
+{
+    TreeNode *node = new TreeNode(num);
+    TreeNode *ptr = root;
+    while (ptr != NULL)
+    {
+        if (node->value < ptr->value)
+        {
+            if (ptr->left == NULL)
+            {
+                ptr->left = node;
+                break;
+            }
+            else
+                ptr = ptr->left;
+        }
+        else if (node->value >= ptr->value)
+        {
+            if (ptr->right == NULL)
+            {
+                ptr->right = node;
+                break;
+            }
+            else
+            {
+                ptr = ptr->right;
+            }
+        }
+    }
+}
+
+void BinarySearchTree::iOTraverse(TreeNode *node)
+{
+    if (node == NULL)
+        return;
+    iOTraverse(node->left);
+    std::cout << node << " ";
+    iOTraverse(node->right);
+}
+
+int main()
+{
+    BinarySearchTree bst{9};
+    bst.insert(4);
+    bst.insert(20);
+    bst.inOrderTraverse();
+    return 1;
+}
