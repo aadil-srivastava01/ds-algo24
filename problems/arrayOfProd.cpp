7b798292325d97dbccf79c69e87faba5c3553462
Aadil Srivastava <aadil.srivastava01@gmail.com>
2024-04-04 22:01:26 +0530

    Added new solution

--- problems/arrayOfProd.cpp
+++ problems/arrayOfProd.cpp
@@ -1,6 +1,7 @@
 /* Problem Statement:
 Write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal to the product of every other number in the input array.
-In other words, the value at output [i] is equal to the product of every number in the input array other than input[i] Note that you're expected to solve this problem without using division.
+In other words, the value at output [i] is equal to the product of every number in the input array other than input[i].`
+Note that you're expected to solve this problem without using division.
 Sample Input
 array [5, 1, 4, 2]
 Sample Output
@@ -11,12 +12,13 @@ Sample Output
 // 20 is equal to 5 x 1 x 4
 
 Solution Complexity:
-
+Time: O(n) | Space: O(n)
 */
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -34,8 +36,50 @@ vector<int> arrayOfProducts(vector<int> array)
     return array;
 }
 
-int main()
+void prodCalcRight(vector<int> &array, unordered_map<int, int> &table, int idx)
 {
 
+    if (idx == array.size() - 1)
+        table[idx] = 1;
+    else
+    {
+        table[idx] = array.at(idx + 1) * table[idx + 1];
+    }
+    return;
+}
+
+void prodCalcLeft(vector<int> &array, unordered_map<int, int> &table, int idx)
+{
+
+    if (idx == 0)
+        table[idx] = 1;
+    else
+    {
+        table[idx] = array.at(idx - 1) * table[idx - 1];
+    }
+    return;
+}
+
+vector<int> arrayOfProductsDS(vector<int> array)
+{
+    unordered_map<int, int> rightable;
+    unordered_map<int, int> leftable;
+    vector<int> result;
+    for (int i = static_cast<int>(array.size()) - 1; i >= 0; i--)
+    {
+        prodCalcRight(array, rightable, i);
+    }
+    for (int i = 0; i < array.size(); i++)
+    {
+        prodCalcLeft(array, leftable, i);
+        result.emplace_back(leftable[i] * rightable[i]);
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> input = {5, 1, 4, 2};
+    vector<int> actual = arrayOfProductsDS(input);
     return 0;
 }
